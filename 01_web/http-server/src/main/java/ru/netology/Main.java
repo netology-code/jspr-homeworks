54f9e58bb0435109f40ab8b68e597930dba627e7
Azamat Kulbarakov <mr.kulbarakov@gmail.com>
2022-04-11 08:51:50 +0500

    01-web task1 added

---
+++ 01_web/http-server/src/main/java/ru/netology/Main.java
@@ -0,0 +1,10 @@
+package ru.netology;
+
+public class Main {
+  public static void main(String[] args) {
+    Server myServer = new Server();
+    myServer.listen();
+  }
+}
+
+
---
+++ 01_web/http-server/src/main/java/ru/netology/Server.java
@@ -0,0 +1,103 @@
+package ru.netology;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.concurrent.Executors;
+
+public class Server {
+
+    private final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+
+    public void listen() {
+        var poolExecutor = Executors.newFixedThreadPool(64);
+
+        try (final var serverSocket = new ServerSocket(9999)) {
+            log("Server start!");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                poolExecutor.submit(() -> handle(socket));
+            }
+        } catch (IOException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
+    public void handle(Socket socket) {
+        try (final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             final var out = new BufferedOutputStream(socket.getOutputStream());
+        ) {
+            log("client connected: " + socket.getRemoteSocketAddress());
+            // read only request line for simplicity
+            // must be in form GET /path HTTP/1.1
+            final var requestLine = in.readLine();
+            final var parts = requestLine.split(" ");
+
+            if (parts.length != 3) {
+                // just close socket
+                log("Invalid request");
+                return;
+            }
+
+            final var path = parts[1];
+            if (!validPaths.contains(path)) {
+                out.write((
+                        "HTTP/1.1 404 Not Found\r\n" +
+                                "Content-Length: 0\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.flush();
+                return;
+            }
+
+            final var filePath = Path.of("http-server", "public", path);
+            final var mimeType = Files.probeContentType(filePath);
+//          final var mimeType = "text/plain";
+            // special case for classic
+            if (path.equals("/classic.html")) {
+                final var template = Files.readString(filePath);
+                final var content = template.replace(
+                        "{time}",
+                        LocalDateTime.now().toString()
+                ).getBytes();
+                out.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + mimeType + "\r\n" +
+                                "Content-Length: " + content.length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                out.write(content);
+                out.flush();
+                log("classic.html loaded");
+                return;
+            }
+
+            final var length = Files.size(filePath);
+            out.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            Files.copy(filePath, out);
+            log(filePath.getFileName() + " loaded");
+            out.flush();
+        } catch (IOException e) {
+            e.printStackTrace(System.out);
+        }
+    }
+
+    private void log(String message) {
+        System.out.println("[" + Thread.currentThread().getName() + "] " + message);
+    }
+}
