ec3a66e8077037bd6648f919544bbeb72a9da1bd
Azamat Kulbarakov <mr.kulbarakov@gmail.com>
2022-06-03 16:34:57 +0500

    feature/di-java

--- 04_servlets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
+++ 04_servlets/servlets/src/main/java/ru/netology/servlet/MainServlet.java
@@ -1,6 +1,7 @@
 package ru.netology.servlet;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import ru.netology.config.JavaConfig;
 import ru.netology.controller.PostController;
 import ru.netology.exception.NotFoundException;
 import ru.netology.service.PostService;
@@ -14,7 +15,7 @@ public class MainServlet extends HttpServlet {
 
     @Override
     public void init() {
-        try (var context = new AnnotationConfigApplicationContext("ru.netology")) {
+        try (var context = new AnnotationConfigApplicationContext(JavaConfig.class)) {
 
             controller = (PostController) context.getBean("postController");
 
